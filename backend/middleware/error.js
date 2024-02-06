cdf9b4d3a19fd35f1cff83f892a75bb3c65562a7
SkullRex001 <honestaditya@gmail.com>
2024-02-06 22:21:20 +0530

    error handling

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -11,6 +11,34 @@ module.exports = (err , req , res , next)=>{
         err = new ErrorHandler(message , 400)
     }
 
+    //Mongoose duplicate key error, ie: trying to register with same email
+
+    if(err.code === 11000) {
+        const duplicateKeys = Object.keys(err.keyValue).join(', ');
+        const message = `Duplicate ${duplicateKeys} Entered`
+        err = new ErrorHandler(message , 400)
+    }
+
+
+    //Wrong JWT error
+
+    if(err.name === 'JsonWebTokenError') {
+        const message = `JSON Web Token is invalid , try again`;
+        err = new ErrorHandler(message , 400)
+    }
+
+    //JWT expire error 
+    if(err.name === "TokenExpiredError" ) {
+        const message = `JSON Web Token has expired`
+
+        err = new ErrorHandler(message , 400)
+    }
+
+
+
+
+
+
     res.status(err.statusCode).json({
         success : false,
         message : err.message
