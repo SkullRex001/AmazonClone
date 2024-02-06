42259b26ef509bb171e0ea4ec2154f2ecf1ba6e0
SkullRex001 <honestaditya@gmail.com>
2024-02-06 23:18:20 +0530

    get User Profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -147,5 +147,13 @@ exports.resetPassword = catchAsyncError(async (req , res , next)=>{
 })
 
 
+exports.getUserDetails = catchAsyncError(async(req , res, next)=>{
+    const user = await User.findById(req.user.id);
+    res.status(200).json({
+        success : true,
+        user
+    })
+})
+
  
 
--- backend/routes/userRour.js
+++ backend/routes/userRour.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const { reginterUser, loginUser, logout, forgotPassword, resetPassword } = require('../controllers/userController')
+const { reginterUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails } = require('../controllers/userController')
+const { isAuthenticatedUser } = require('../middleware/auth')
 const router = express()
 
 router.route("/register").post(reginterUser)
@@ -7,4 +8,6 @@ router.route("/login").post(loginUser)
 router.route("/logout").get(logout)
 router.route("/password/forgot").post(forgotPassword)
 router.route("/password/reset/:token").put(resetPassword)
+router.route("/me").get(isAuthenticatedUser
+     , getUserDetails)
 module.exports = router;
